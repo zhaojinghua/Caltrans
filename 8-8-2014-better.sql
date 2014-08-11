9bd10884a6dea6ed127de604dd47a6ca9ac71b97
zhaojinghua <zhaojinghua@hotmail.com>
2014-08-11 13:46:37 -0700

    color budget group and summary

--- 8-8-2014-better.sql
+++ 8-8-2014-better.sql
@@ -1,1 +1,19 @@
-select year, ea, (select count(*) from excelnew ee where ee.ea=eee.ea) as rec_count, ca_projectid, county, route, rs, bpp, beg_pm, bps, epp, end_pm, eps, dir, lane, estimated_cost from excelnew eee order by ea; 
+select year, 
+    ea, 
+    (select count(*) from excelnew ee where ee.ea=eee.ea) as rec_count, 
+    ca_projectid, 
+    county, 
+    route, 
+    rs, 
+    bpp, 
+    beg_pm, 
+    bps, epp, 
+    end_pm, 
+    eps, 
+    dir, 
+    lane, 
+    estimated_cost 
+    from excelnew eee order by ea; 
+    
+    
+select * from s1318;
