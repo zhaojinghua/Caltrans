dd003580631d8f01bb990f55a2f6483842e9261b
zhaojinghua <zhaojinghua@hotmail.com>
2014-08-24 17:37:56 -0700

    Gantt Chart Report
    F11, ALT+F1 to generate chart; Using Macro to generate chart, using
    formula to extract required informaiton(concatenate and manipulate n
    rows to one cell), using text to columns to split one cell to many

---
+++ for Hector/rawdataforganttchart.sql
@@ -0,0 +1,11 @@
+select county, route, max(end_pm - beg_pm) as range from CONSTRUCTIONHISTORYPM where county='SON' and route='001' group by county, route order by range desc, county, route;
+
+select distinct county, route, year, beg_pm, end_pm, (end_pm-beg_pm) as length from CONSTRUCTIONHISTORYPM where county='SON' and route='001' order by county, route, year;
+
+
+select b.ea, b.treatment, b.county, b.route, b.year, 
+  b.beg_pm, b.end_pm, (b.end_pm-b.beg_pm) as length 
+  from CONSTRUCTIONHISTORYPM b 
+  where b.county='SON' and b.route='001' and b.dir='Right'
+  order by b.year, b.beg_pm, end_pm;
+  
