a7088d6ff6d5cb16d5b87f0f1b229bbbd3b7ef2b
zhaojinghua <zhaojinghua@hotmail.com>
2014-08-25 21:58:01 -0700

    two more test files and sql to find a good one

--- for Hector/rawdataforganttchart.sql
+++ for Hector/rawdataforganttchart.sql
@@ -6,6 +6,12 @@ select distinct county, route, year, beg_pm, end_pm, (end_pm-beg_pm) as length f
 select b.ea, b.treatment, b.county, b.route, b.year, 
   b.beg_pm, b.end_pm, (b.end_pm-b.beg_pm) as length 
   from CONSTRUCTIONHISTORYPM b 
-  where b.county='SON' and b.route='001' and b.dir='Right'
+  where b.county='SBD' and b.route='062' and b.dir='Right'
   order by b.year, b.beg_pm, end_pm;
+  
+  
+select county, route, count(year) as num
+  from CONSTRUCTIONHISTORYPM
+  group by county, route
+  order by num desc, county, route;  
   
