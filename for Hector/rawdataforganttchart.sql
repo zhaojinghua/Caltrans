e1a455a87d4c1f7957b10f273575b24c446c10c8
zhaojinghua <zhaojinghua@hotmail.com>
2014-08-30 22:31:14 -0700

    add begpm endpm for label good for Pivot
    sql can generate beg pm **** for labels

--- for Hector/rawdataforganttchart.sql
+++ for Hector/rawdataforganttchart.sql
@@ -10,6 +10,11 @@ select b.ea, b.treatment, b.county, b.route, b.year,
   where b.county='SBD' and b.route='062' and b.dir='Right'
   order by b.year, b.beg_pm, end_pm;
   
+select b.ea, b.treatment, b.county, b.route, b.year, 
+  ('Beg PM: ' || to_char(b.beg_pm, 990.999)) as "Beg PM", ('End PM: ' || to_char(b.end_pm, 990.999)) as "End PM", (b.end_pm-b.beg_pm) as length 
+  from CONSTRUCTIONHISTORYPM b 
+  where b.county='SBD' and b.route='062' and b.dir='Right'
+  order by b.year, b.beg_pm, end_pm;
   
 select county, route, count(year) as num
   from CONSTRUCTIONHISTORYPM
