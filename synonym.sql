a1a8a65e8a747af27a63964e92dbf098ea82dbbf
zhaojinghua <zhaojinghua@hotmail.com>
2014-09-05 13:47:08 -0700

    synonym sql

---
+++ synonym.sql
@@ -0,0 +1,17 @@
+create synonym pms_analysis_scenario_wp for pms_ca.pms_analysis_scenario_wp;
+
+create synonym ms_treatment for pms_ca.ms_treatment;
+
+create synonym pms_analysis_scenario for pms_ca.pms_analysis_scenario;
+
+create synonym pms_budget_cat for pms_ca.pms_budget_cat;
+
+create synonym setup_ca_county for pms_ca.setup_ca_county;
+
+create synonym setup_column_id for pms_ca.setup_column_id;
+
+create synonym setup_lane_dir for pms_ca.setup_lane_dir;
+
+create synonym setup_loc_ident for pms_ca.setup_loc_ident;
+
+create synonym setup_mwp_project_status for pms_ca.setup_mwp_project_status;
